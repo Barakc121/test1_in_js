399af1c4362471f3ca5ec0e37189e6766e64e6a7
Barak <barakc121@gmail.com>
2025-12-04 11:20:39 +0200

    next

--- riddle_game/app.js
+++ riddle_game/app.js
@@ -4,7 +4,7 @@ import r2 from "./riddles/r2";
 import r3 from "./riddles/r3";
 import r4 from "./riddles/r4";
 import r5 from "./riddles/r5";
-import create from "utils/create"
-const name = input("What is your name? ");
-console.log(`Hello, ${name}!`);
-you_name("barak")
+let a=r1,r2,r3,r4,r5
+for(i=0,i <a.length,i++)
+    console.log(object)
+
--- utils/create.js
+++ utils/create.js
@@ -1,6 +1,31 @@
 import input from "analiza-sync";
-function you_name(){
-    const name=input("what is your name")
-    return name
+const startTime = new Date().getTime(); 
+
+function player(name=input("what is your name  :")){
+
+    let players =[]
+    const men={name:name,times:[]}
+    players.push(men)
+    return players
 }
-console.log(you_name())
+console.log(player())
+
+function createPlayer(name){
+    if(name=== "string" && name !==Number)
+        return name
+}
+// console.log(createPlayer(player))
+
+function addSolveTime(player, seconds){
+    const endTime = new Date().getTime();
+    const timeTaken = endTime - startTime;
+    const avg=timeTaken+ player
+
+    return {timeTaken,avg}
+}
+console.log(addSolveTime())
+
+
+function showStats(player){
+
+}
