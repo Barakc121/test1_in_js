e4ef7413464c7208227c56f54a3af0339c5a4b2d
Barak <barakc121@gmail.com>
2025-12-04 11:36:44 +0200

    next 1

--- riddle_game/app.js
+++ riddle_game/app.js
@@ -1,10 +1,11 @@
-import input from "analiza-sync";
-import r1 from "./riddles/r1";
-import r2 from "./riddles/r2";
-import r3 from "./riddles/r3";
-import r4 from "./riddles/r4";
-import r5 from "./riddles/r5";
-let a=r1,r2,r3,r4,r5
-for(i=0,i <a.length,i++)
-    console.log(object)
+import r1 from "./riddles/r1.js";
+import r2 from "./riddles/r2.js";
+import r3 from "./riddles/r3.js";
+import r4 from "./riddles/r4.js";
+import r5 from "./riddles/r5.js";
 
+console.log(r1)
+console.log(r2)
+console.log(r3)
+console.log(r4)
+console.log(r5)
